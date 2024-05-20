b9decac1c9a7dec55bb71dc41d982aba07138ef8
MrCrazyVampire <lashya.r.saxena@gmail.com>
2024-05-20 20:04:23 +0530

    Final- v1

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 	var hamburger = document.querySelector(".hamburger");
 	var menu = document.querySelector("nav ul");
-
+	var btnn = document.querySelector(".button-container");
 	hamburger.addEventListener("click", function () {
 		// Toggle menu visibility
 		menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 	});
+	hamburger.addEventListener("click", function () {
+		// Toggle menu visibility
+		btnn.style.display = btnn.style.display === "none" ? "flex" : "none";
+	});
+
 
 	var slideIndex = 1; // Start with the first slide index
 	showSlides(slideIndex); // Initialize the slideshow with the first slide
